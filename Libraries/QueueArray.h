6abad82a218f2368a910675d4e0cfb665a77aaa3
AbdSh17 <154285986+AbdSh17@users.noreply.github.com>
2024-06-23 22:52:14 +0300

    Sample Main

--- Libraries/QueueArray.h
+++ Libraries/QueueArray.h
@@ -74,3 +74,30 @@ int getFront(queueArray q)
     
     return q->array[q->front];
 }
+
+/*
+
+int main()
+{
+    // Enter the size ;
+    queueArray q = makeEmptyQueueArray(4);
+    enqueueArray(5, q);
+    enqueueArray(6, q);
+    enqueueArray(1, q);
+
+    dequeueArray(q);
+
+    for (int i = 0, j = q->front; i < q->size; i++, j++)
+    {
+        if (j == q->capacity)
+        {
+            j = 0;
+        }
+        
+        printf("%d  ", q->array[j]);
+    }
+    // Use the remain functions ;
+    return 0;
+}
+
+*/
